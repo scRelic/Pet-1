cd73ebe09715313395f860ebda0e28ce20c7d6dd
Дмитрий Богуцкий <dmitriubogutskiu2@gmail.com>
2025-12-19 19:14:11 +0200

    Connect to DB

--- server/src/db.ts
+++ server/src/db.ts
@@ -5,11 +5,13 @@ dotenv.config();
 const { Pool } = pg;
 
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT) || 5432,
+  connectionString: process.env.DB_URL,
+  ssl: { rejectUnauthorized: false },
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
+  // port: Number(process.env.DB_PORT) || 5432,
 });
 
 pool.on("error", (err) => {
